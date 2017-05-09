8d19d206aa8a8736fec799f562543ef044ede842
Julian Schelker <julian.schelker@adnovum.ch>
2017-05-09 15:47:37 +0200

    disable testing logs

--- source/constants.lua
+++ source/constants.lua
@@ -5,6 +5,6 @@ fullModName = "integratedCircuitry" -- required for logging and prototypes
 
 
 libLog.debug_master = true
-libLog.testing = true
+--libLog.testing = true
 --libLog.testing = false -- enables player printing of every log, sets log level to info
 --libLog.debug_level = 1  -- 1=info 2=warn 3=error
