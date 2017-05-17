18707182cc0f418da9688d0ae84310b6da319313
judos <judos@gmx.ch>
2017-05-17 19:27:59 +0200

    add memory combinator wip

--- source/data.lua
+++ source/data.lua
@@ -2,3 +2,4 @@ require "libs.all"
 
 require "prototypes.lamp-panel"
 require "prototypes.status-panel"
+require "prototypes.memory-combinator"
--- source/prototypes/memory-combinator.lua
+++ source/prototypes/memory-combinator.lua
@@ -6,7 +6,7 @@ overwriteContent(memory, {
 	name = "memory-combinator",
 	order = "z[small-lamp]0",
 	place_result = "memory-combinator",
-	icon = "__integratedCircuitry__/graphics/icons/lamp-panel.png",
+	icon = "__integratedCircuitry__/graphics/icons/memory-combinator.png",
 })
 data:extend({	memory })
 
@@ -17,9 +17,8 @@ data:extend({
 		name = "memory-combinator",
 		enabled = false,
 		ingredients = {
-			{"constant-combinator", 1},
-			{"decider-combinator", 2},
-			{"arithmetic-combinator", 1},
+			{"constant-combinator", 4},
+			{"electronic-circuit", 20},
 		},
 		result = "memory-combinator"
 	}
@@ -27,12 +26,39 @@ data:extend({
 
 
 -- Entity
-local memory = deepcopy(data.raw["combinator"]["constant-combinator"])
+local remove = {}
+local memory = deepcopy(data.raw["lamp"]["small-lamp"])
 overwriteContent(memory, {
 	name = "memory-combinator",
-	energy_usage_per_tick = "150KW",
-	
-})
+	icon = "__integratedCircuitry__/graphics/icons/memory-combinator.png",
+	max_health = 500,
+	collision_box = {{-0.85, -0.85}, {0.85, 0.85}},
+	selection_box = {{-1, -1}, {1, 1}},
+	energy_usage_per_tick = "100KW",
+	picture_off = {
+		filename = "__integratedCircuitry__/graphics/entity/memory-combinator.png",
+		x = 0,
+		y = 0,
+		width = 120,
+		height = 120,
+		frame_count = 1,
+		shift = {0,0}, --{0.140625, 0.140625},
+	},
+	picture_on = {
+		filename = "__integratedCircuitry__/graphics/empty.png",
+		width = 1,
+		height = 1,
+	},
+	light = remove,
+	light_when_colored = remove,
+	glow_size = 0,
+	glow_color_intensity = 0,
+	sprites = remove,
+	activity_led_light_offsets = remove,
+	activity_led_sprites = remove,
+	input_connection_bounding_box = {{-1, -1}, {0, 0}},
+	output_connection_bounding_box = {{0, 0}, {1,1}},
+},remove)
 
 memory.minable.result = "memory-combinator"
 data:extend({	memory })
