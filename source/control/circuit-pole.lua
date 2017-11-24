7bcc0dce4c780bc77453aba63a2dcc0cdacb5954
judos <judos@gmx.ch>
2017-11-24 19:19:55 +0100

    wire fixes if other poles are built

--- source/control/circuit-pole.lua
+++ source/control/circuit-pole.lua
@@ -7,17 +7,34 @@ entities["circuit-pole"] = circuitPole
 -- Constants
 local m = {} --used for methods of the circuitPole
 
----------------------------------------------------
--- entityData
----------------------------------------------------
-
--- Used data:
--- {
--- }
-
---------------------------------------------------
--- Global data
---------------------------------------------------
+function circuitPole_build(entity)
+	if entity.type ~= "electric-pole" then
+		return
+	end
+	
+	-- diconnect all circuit poles
+	for k,e in pairs(entity.neighbours.copper) do
+		if e.name == "circuit-pole" then
+			entity.disconnect_neighbour(e)
+		end
+	end
+	-- make some connnections if all have been removed
+	if # entity.neighbours.copper > 0 then
+		return
+	end
+	
+	local searchArea = {{entity.position.x-20, entity.position.y-20}, {entity.position.x+20, entity.position.y+20}}
+	local entities = entity.surface.find_entities_filtered{type="electric-pole",area = searchArea}
+	for _,e in pairs(entities) do
+		if e.name ~= "circuit-pole" then
+			entity.connect_neighbour(e)
+		end
+		if # entity.neighbours.copper >= 2 then
+			break
+		end
+	end
+	
+end
 
 
 ---------------------------------------------------
