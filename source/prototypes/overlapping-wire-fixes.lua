3a98f8e41b8f97ae8c929bb2c39932c52ae87d2c
judos <judos@gmx.ch>
2017-11-24 18:25:36 +0100

    add wire fix nil checks

--- source/prototypes/overlapping-wire-fixes.lua
+++ source/prototypes/overlapping-wire-fixes.lua
@@ -5,11 +5,14 @@ local belt_offset_x = 0.02
 local chest_offset_x = 0.04
 
 function updateWireConnectionPoints(entity, offsetX)
-	print(entity.name)
 	local connections = entity.circuit_wire_connection_point
 	if connections ~= nil then
-		connections.shadow.red[1] = connections.shadow.red[1] + offsetX
-		connections.wire.red[1] = connections.wire.red[1] + offsetX
+		if connections.shadow ~= nil and connections.shadow.red ~= nil then
+			connections.shadow.red[1] = connections.shadow.red[1] + offsetX
+		end
+		if connections.wire ~= nil and connections.wire.red ~= nil then
+			connections.wire.red[1] = connections.wire.red[1] + offsetX
+		end
 	end
 end
 
