b9536a400126723a4d2f1f032f12a099841ea4e4
Julian Schelker <judos@gmx.ch>
2020-02-04 01:22:38 +0100

    make compact combinator fully blueprintable

--- source/prototypes/compact-combinator.lua
+++ source/prototypes/compact-combinator.lua
@@ -30,7 +30,7 @@ data:extend({
 local entity = table.deepcopy(data.raw["container"]["iron-chest"])
 overwriteContent(entity, {
 	name = "compact-combinator",
-	collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
+	collision_box = {{-0.45, -0.45}, {0.45, 0.45}},
   selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
 	inventory_size = 0,
 	picture = {
@@ -54,17 +54,27 @@ entity.fast_replaceable_group = nil
 entity.minable.result = "compact-combinator"
 data:extend({	entity })
 
+-- Technology
+addTechnologyUnlocksRecipe("circuit-network","compact-combinator")
+
+
 
 -- Port
 local entity = table.deepcopy(data.raw["electric-pole"]["small-electric-pole"])
 overwriteContent(entity, {
 	name = "compact-combinator-io",
-	collision_box = {{-0.25, -0.25}, {0.25, 0.25}},
+	collision_box = {{-0.20, -0.20}, {0.20, 0.20}},
   selection_box = {{-0.25, -0.25}, {0.25, 0.25}},
 	order="a",
 	track_coverage_during_build_by_moving = false,
 	supply_area_distance = 0,
-	flags = {"hidden", "placeable-neutral", "placeable-off-grid", "not-on-map"},
+	flags = {
+		"placeable-player",
+		"placeable-enemy",
+		"player-creation",
+		"placeable-off-grid",
+		"not-on-map"
+	},
 	connection_points = {
 		{
 			shadow = {
@@ -89,9 +99,16 @@ overwriteContent(entity, {
 	
 entity.circuit_connector_sprites = nil
 entity.fast_replaceable_group = nil
-entity.minable.result = nil
+entity.minable.result = "compact-combinator-io"
 data:extend({	entity })
 
-
-addTechnologyUnlocksRecipe("circuit-network","compact-combinator")
-
+-- Item
+local item = table.deepcopy(data.raw["item"]["iron-chest"])
+overwriteContent(item, {
+	name = "compact-combinator-io",
+	subgroup = "circuit-network",
+	order = "c[combinators]-e[compact-combinator]",
+	place_result = "compact-combinator-io",
+	icon = "__integratedCircuitry__/graphics/icons/compact-combinator.png",
+})
+data:extend({	item })
