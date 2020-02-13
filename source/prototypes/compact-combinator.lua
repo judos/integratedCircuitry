1c3d823b5bff1ba3dfa18de4ce75867b0048925b
Julian Schelker <judos@gmx.ch>
2020-02-13 22:29:52 +0100

    improve performence a lot by changing connection poles into invisible chests

--- source/prototypes/compact-combinator.lua
+++ source/prototypes/compact-combinator.lua
@@ -176,22 +176,18 @@ data:extend({	item })
 
 -- Big pole for connecting cables outside with inside
 local removeKey = "__REMOVE__"
-local entity = table.deepcopy(data.raw["electric-pole"]["small-electric-pole"])
+local entity = table.deepcopy(data.raw["container"]["steel-chest"])
 overwriteContent(entity, {
 	name = "compact-combinator-connection",
 	collision_box = removeKey, -- {{0, 0}, {0, 0}},
 	selection_box = removeKey, --{{-0.25, -0.25}, {0.25, 0.25}},
 	collision_mask = {},
 	order="a",
-	track_coverage_during_build_by_moving = false,
-	supply_area_distance = 0,
-	maximum_wire_distance = 64,
+	circuit_wire_max_distance = 64,
 	draw_circuit_wires = false,
 	draw_copper_wires = false,
 	flags = {
 		"placeable-player",
-		"placeable-enemy",
-		"player-creation",
 		"placeable-off-grid",
 		"not-on-map",
 		"not-blueprintable",
@@ -201,23 +197,16 @@ overwriteContent(entity, {
 		"hidden",
 		"not-rotatable"
 	},
-	pictures = {
+	picture = {
 		filename = "__integratedCircuitry__/graphics/empty4x1.png",
 		priority = "extra-high",
 		width = 1,
 		height = 1,
-		direction_count = 1,
 		shift = {0, 0}
 	},
-	connection_points = {
-		{
-			shadow = { red = {0, 0.1}, green = {0.05, 0.15} },
-			wire = { red = {0, 0}, green = {0.05, 0.05} }
-		}
-	},
+	circuit_connector_sprites = removeKey,
+	fast_replaceable_group = removeKey,
 }, removeKey)
-entity.circuit_connector_sprites = nil
-entity.fast_replaceable_group = nil
 entity.minable.result = nil
 data:extend({	entity })
 
